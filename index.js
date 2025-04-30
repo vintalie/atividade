4005d89af9793a7bf99c6adb090c5cd9eb31258b
vintalie <marioluizsan@gmail.com>
2025-04-30 12:38:57 -0300

    sessões adicionadas

--- index.js
+++ index.js
@@ -4,9 +4,20 @@ const app = express();
 const ejs = require('ejs');
 const path = require('path');
 const routes = require('./routes')
+const session = require('express-session')
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+app.use(session({
+    secret:"Bambole-tringular-do-hiper-espaço-sideral",
+    resave:true,
+    saveUninitialized:true,
+}))
+// Enable parsing of JSON request bodies
+app.use(express.json());
+
+// Enable parsing of URL-encoded request bodies
+app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'assets')))
 
