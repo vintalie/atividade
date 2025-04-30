8670e8dc5daa531db70d22c14b26f3af8f0e54f1
vintalie <marioluizsan@gmail.com>
2025-04-30 12:39:22 -0300

    criada as rotas do login,, registro e dashboard

--- routes.js
+++ routes.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-const homeController = require('./controllers/homeController')
-const contatoController = require('./controllers/contatoController')
-// const musicaController = require('./controllers/musicaController')
-// const integrantesController = require('./controllers/integrantesController')
+const homeController = require('./controllers/homeController');
+const loginController = require('./controllers/loginController');
+const registerController = require('./controllers/registerController');
+const dashboardController = require('./controllers/dashboardController');
+
 
 router.get('/', homeController.get)
-router.get('/contato', contatoController.get)
+
+router.get('/entrar', loginController.get)
+router.post('/entrar', loginController.post)
+
+router.get('/criar-conta', registerController.get)
+router.post('/criar-conta', registerController.post)
+
+router.get('/dashboard', dashboardController.get)
+router.get('/dashboard', dashboardController.post)
 // router.get('/musica', musicaController.get)
 // router.get('/integrantes', integrantesController.get)
 
