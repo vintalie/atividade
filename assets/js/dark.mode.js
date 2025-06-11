43bbc25e4ff04508eefdde7032de81db5b5ed7f0
Miguereles <miguelpesqueirafreitas@gmail.com>
2025-06-11 03:23:20 -0300

    apliquei uma parte do dark mode

--- assets/js/dark.mode.js
+++ assets/js/dark.mode.js
@@ -1,4 +1,17 @@
-
+// Ao clicar no botão, alterna o modo e salva a preferência
 document.getElementById('toggle-dark').onclick = function () {
     document.body.classList.toggle('dark-mode');
+    // Salva a preferência no localStorage
+    if(document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('dark-mode', 'on');
+    } else {
+        localStorage.setItem('dark-mode', 'off');
+    }
+}
+
+// Ao carregar a página, aplica o modo salvo
+window.onload = function() {
+    if(localStorage.getItem('dark-mode') === 'on') {
+        document.body.classList.add('dark-mode');
+    }
 }
